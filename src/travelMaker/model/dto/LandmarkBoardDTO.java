59f48175b350bb7212472e1f1e8bc904e50589b9
ChoYoonHo <yoonhocho91@gmail.com>
2021-03-19 15:31:45 +0900

    랜드마크DTO 좋아요DTO 추가

--- src/travelMaker/model/dto/LandmarkBoardDTO.java
+++ src/travelMaker/model/dto/LandmarkBoardDTO.java
@@ -1,5 +1,88 @@
 package travelMaker.model.dto;
 
+import java.sql.Timestamp;
+
 public class LandmarkBoardDTO {
 
+	private Integer lNo;
+	private String writer;
+	private String lName;
+	private String lType;
+	private String addr;
+	private Integer xLoc;
+	private Integer yLoc;
+	private String lCont;
+	private Integer lOpen;
+	private Integer likedCnt;
+	private Timestamp reg;
+	
+	
+	public Integer getlNo() {
+		return lNo;
+	}
+	public void setlNo(Integer lNo) {
+		this.lNo = lNo;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getlType() {
+		return lType;
+	}
+	public void setlType(String lType) {
+		this.lType = lType;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public Integer getxLoc() {
+		return xLoc;
+	}
+	public void setxLoc(Integer xLoc) {
+		this.xLoc = xLoc;
+	}
+	public Integer getyLoc() {
+		return yLoc;
+	}
+	public void setyLoc(Integer yLoc) {
+		this.yLoc = yLoc;
+	}
+	public String getlCont() {
+		return lCont;
+	}
+	public void setlCont(String lCont) {
+		this.lCont = lCont;
+	}
+	public Integer getlOpen() {
+		return lOpen;
+	}
+	public void setlOpen(Integer lOpen) {
+		this.lOpen = lOpen;
+	}
+	public Integer getLikedCnt() {
+		return likedCnt;
+	}
+	public void setLikedCnt(Integer likedCnt) {
+		this.likedCnt = likedCnt;
+	}
+	public Timestamp getReg() {
+		return reg;
+	}
+	public void setReg(Timestamp reg) {
+		this.reg = reg;
+	}
+	
+	
 }
--- src/travelMaker/model/dto/LandmarkLikedDTO.java
+++ src/travelMaker/model/dto/LandmarkLikedDTO.java
@@ -1,5 +1,25 @@
 package travelMaker.model.dto;
 
+
 public class LandmarkLikedDTO {
 
+	private Integer lNo;
+	private String id;
+	
+	
+	public Integer getlNo() {
+		return lNo;
+	}
+	public void setlNo(Integer lNo) {
+		this.lNo = lNo;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
 }
