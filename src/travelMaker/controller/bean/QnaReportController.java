ad7b1fb3a8d120c11fb3f44071990fc501fe6403
smlee <vic_sm@naver.com>
2021-03-19 11:54:10 +0900

    controller

--- src/travelMaker/controller/bean/QnaReportController.java
+++ src/travelMaker/controller/bean/QnaReportController.java
@@ -1,10 +1,21 @@
 package travelMaker.controller.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import travelMaker.service.bean.QnaReportService;
 
 @Controller
 @RequestMapping("/qr/")
 public class QnaReportController {
+	
+	@Autowired
+	private QnaReportService qnaReportService = null;
+	
+	@RequestMapping("reportWrite.tm")
+	public String reportWrite() {
+		return "client/report/reportWriteForm";
+	}
 
 }
