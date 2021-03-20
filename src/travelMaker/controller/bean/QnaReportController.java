aa499033b4ed42571d891ee99920fb10bef3a30b
smlee <vic_sm@naver.com>
2021-03-20 18:21:01 +0900

    신고셀렉트추가

--- src/travelMaker/controller/bean/QnaReportController.java
+++ src/travelMaker/controller/bean/QnaReportController.java
@@ -1,9 +1,11 @@
 package travelMaker.controller.bean;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import travelMaker.model.dto.ReportReasonDTO;
@@ -16,27 +18,30 @@ public class QnaReportController {
 	@Autowired
 	private QnaReportServiceImpl qnaReportService = null;
 	
+	@RequestMapping("reasonInsert.tm")
+	public String reasonInsert(ReportReasonDTO dto) throws SQLException {		
+		qnaReportService.insertReason(dto);		
+		return "redirect:reason.tm";
+	}
+	
 	@RequestMapping("reportWriteForm.tm")
-	public String reportWriteForm() {
+	public String reportWriteForm(int rType, Model model) throws SQLException {		
+		List res = qnaReportService.selectReason(rType);
+		model.addAttribute("res", res);
+		model.addAttribute("rType", rType);
 		return "client/report/reportWriteForm";
 	}
 	
 	@RequestMapping("reportWritePro.tm")
-	public String reportWritePro() {
+	public String reportWritePro(int rNo) {
 		return "client/report/reportWritePro";
 	}
 	
 	@RequestMapping("reason.tm")
-	public String reason() {
+	public String reason(Model model) throws SQLException {
+		List res = qnaReportService.selectReasonAll();
+		model.addAttribute("res", res);
 		return "admin/qnaReport/reason";
-	}
-	
-	@RequestMapping("reasonInsert.tm")
-	public String reasonInsert(ReportReasonDTO dto) throws SQLException {
-		
-		qnaReportService.addReason(dto);
-		
-		return "redirect:reason.tm";
 	}
 
 }
