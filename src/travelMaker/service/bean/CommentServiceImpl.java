8dcde0ec157f8cddd2f0780b90419d3880b3b93b
brjeong <brjeong0@gmail.com>
2021-03-21 01:21:04 +0900

    comment 작업

--- src/travelMaker/service/bean/CommentServiceImpl.java
+++ src/travelMaker/service/bean/CommentServiceImpl.java
@@ -1,5 +1,47 @@
 package travelMaker.service.bean;
 
-public class CommentServiceImpl {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import travelMaker.model.dao.UserCmtDAO;
+import travelMaker.model.dto.GroupSpaceDTO;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+	@Autowired
+	private UserCmtDAO userCmtDAO = null;
+
+	//내가 여행한 모든 여행그룹 가져오기
+	@Override
+	public List getMyGroup(String id) throws SQLException {
+		
+		List myGroupNo = userCmtDAO.myGroupNo(id);		
+		
+		
+		List myGroupInfo = null;
+		//for(int i = 0; i < myGroupNo.size(); i++) {
+		//	GroupSpaceDTO dto = userCmtDAO.getMyGroupInfo(4);
+		//	myGroupInfo.add(dto);
+	//	}
+		
+		
+		
+		return myGroupInfo;
+	}
+
+	
+
+	
+	
+	
+	
+	
+	
 }
