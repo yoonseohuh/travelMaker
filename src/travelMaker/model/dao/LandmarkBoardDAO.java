2169442a514875f702bcc89e015c4cefb578c4c5
ChoYoonHo <yoonhocho91@gmail.com>
2021-03-23 16:16:49 +0900

    지도 검색 완료, 컨트롤러 약 수정

--- src/travelMaker/model/dao/LandmarkBoardDAO.java
+++ src/travelMaker/model/dao/LandmarkBoardDAO.java
@@ -1,11 +1,20 @@
 package travelMaker.model.dao;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 import travelMaker.model.dto.LandmarkBoardDTO;
 
 public interface LandmarkBoardDAO {
-	
-	public void insertLand(LandmarkBoardDTO dto) throws SQLException;
 
+	// 랜드마크 생성 setlikedCnt 0 으로 맞추고 
+	public void insertLand(LandmarkBoardDTO dto) throws SQLException;
+	
+	// 랜드마크 가져오기
+	public LandmarkBoardDTO getLandInfo();
+	// 랜드마크 전체 가져오기
+	public Map getLands() throws SQLException;
+	// 랜드마크 공개범위 전체공개 0, 회원공개 1
+	public void getLandOpen(int lOpen) throws SQLException;
+		
 }
--- src/travelMaker/service/bean/LandmarkServiceImpl.java
+++ src/travelMaker/service/bean/LandmarkServiceImpl.java
@@ -1,9 +1,44 @@
 package travelMaker.service.bean;
 
+import java.sql.SQLException;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
+import travelMaker.model.dto.LandmarkBoardDTO;
+
 @Service
-public class LandmarkServiceImpl {
+public class LandmarkServiceImpl implements LandmarkService{
+
+	@Override
+	public void insertLand(LandmarkBoardDTO dto) throws SQLException {
+		
+		
+	}
+
+	@Override
+	public LandmarkBoardDTO getLandInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map getLands() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addLikedCnt(int lNo, String id) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getLandOpen(int lOpen) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 }
