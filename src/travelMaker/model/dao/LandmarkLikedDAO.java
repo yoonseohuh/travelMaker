11dabd49400c7f31c054f6af911bf6c4561fb35e
ChoYoonHo <yoonhocho91@gmail.com>
2021-03-24 18:21:18 +0900

    landmarkWriteForm 검색창 삭제버튼 추가

--- src/travelMaker/model/dao/LandmarkLikedDAO.java
+++ src/travelMaker/model/dao/LandmarkLikedDAO.java
@@ -6,10 +6,10 @@ import travelMaker.model.dto.LandmarkBoardDTO;
 public interface LandmarkLikedDAO {
 
 	// 랜드마크 고유번호 가져오기
-	public LandmarkBoardDTO getLand(int lNo);
+	public LandmarkBoardDTO getLand();
 	// 랜드마크 좋아요 
-	public void insertLandLiked(int lNo, String id) throws Exception;
+	public void insertLandLiked() throws Exception;
 	// 랜드마크 좋아요 수 올려주기
-	public void addLikedCnt(int lNo, String id) throws SQLException;
+	public void addLikedCnt() throws SQLException;
 	
 }
