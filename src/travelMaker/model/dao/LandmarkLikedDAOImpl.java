22552d865d1e33129168c7de8bda231ada454d51
username <user@email.mail>
2021-03-25 09:41:14 +0900

    오류

--- src/travelMaker/model/dao/LandmarkLikedDAOImpl.java
+++ src/travelMaker/model/dao/LandmarkLikedDAOImpl.java
@@ -25,4 +25,22 @@ public class LandmarkLikedDAOImpl implements LandmarkLikedDAO {
 		
 	}
 
+	@Override
+	public LandmarkBoardDTO getLand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertLandLiked() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addLikedCnt() throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+ 
 }
