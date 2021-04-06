5a440e76fd142a9b12b60393898ecd577036b354
ChoYoonHo <yoonhocho91@gmail.com>
2021-04-06 12:08:56 +0900

    랜드마크 좋아요 약 추가

--- src/travelMaker/controller/bean/LandmarkController.java
+++ src/travelMaker/controller/bean/LandmarkController.java
@@ -1,14 +1,15 @@
 package travelMaker.controller.bean;
 
-import java.sql.SQLException;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestAttributes;
@@ -26,7 +27,7 @@ public class LandmarkController {
 	
 	// 랜드마크 페이지
 	@RequestMapping("landmark.tm")
-	public String landmark(Model model) throws SQLException{
+	public String landmark(Model model) throws Exception{
 		// 고유번호, 작성자, 장소명, 유형, 주소, 소개, 공개범위, x,y값
 		
 		List land = landmarkService.getLands();
@@ -38,7 +39,7 @@ public class LandmarkController {
 	// 랜드마크 페이지 데이터 가져오기 ajax 요청 
 	@RequestMapping("landmarkList.tm")
 	@ResponseBody
-	public List landmarkList() throws SQLException {
+	public List landmarkList() throws Exception {
 		
 		List land = landmarkService.getLands();
 		
@@ -47,7 +48,7 @@ public class LandmarkController {
 	
 	// 랜드마크 클릭 시 랜드마크 가져오기
 	@RequestMapping("landmarkCont.tm")
-	public String landmarkCont(int lNo, Model model) throws SQLException {
+	public String landmarkCont(int lNo, Model model) throws Exception {
 		 LandmarkBoardDTO land =  landmarkService.getLand(lNo);
 		 System.out.println(land);
 		 model.addAttribute("land", land);
@@ -64,7 +65,7 @@ public class LandmarkController {
 	
 	// 랜드마크 프로 페이지
 	@RequestMapping("landWritePro.tm")
-	public String landWritePro(LandmarkBoardDTO dto, Model model) throws SQLException {
+	public String landWritePro(LandmarkBoardDTO dto, Model model) throws Exception {
 		landmarkService.insertLand(dto); // 오토와이어드 해놓은 landmarkService를 쓴다음 고놈이 가지고 있는 insertLand를 사용한다. 
 		model.addAttribute("land", dto); // model은 값이 들어갔는지 확인하기 위해 사용 되는 놈
 		return "client/landmark/landWritePro";
@@ -72,7 +73,7 @@ public class LandmarkController {
 	
 	//나의 랜드마크
 	@RequestMapping("myLand.tm")
-	public String myLand(HttpSession session, Model model) throws SQLException {
+	public String myLand(HttpSession session, Model model) throws Exception {
 		// 구면인데 초면 같으신 분
 		String id = (String)RequestContextHolder.getRequestAttributes().getAttribute("memId", RequestAttributes.SCOPE_SESSION);
 		//내가 작성한 랜마
@@ -80,6 +81,7 @@ public class LandmarkController {
 		model.addAttribute("memId", id);
 		model.addAttribute("myLand", myLand);
 		//내가 좋아요한 랜드마크
+		
 		return "client/mypage/myLand";
 	}
 	
@@ -88,8 +90,9 @@ public class LandmarkController {
 		System.out.println(lNo);
 		return "client/mypage/myLandDelete";
 	}
+	
 	@RequestMapping("myLandDeletePro.tm")
-	public String myLandDeletePro(HttpSession session, String pw, String lNo) throws SQLException{
+	public String myLandDeletePro(HttpSession session, String pw, String lNo) throws Exception{
 		//아이디, 비밀번호 일치하는지 확인하는 메서드 
 		// 맞으면 삭제해주는 메서드
 		//landmarkService.deleteMyLand(pw);
@@ -97,6 +100,17 @@ public class LandmarkController {
 		System.out.println(lNo);
 		return "redirect:/myLand.tm";
 	}
+	@ResponseBody
+	@RequestMapping("landmarkLiked.tm")
+	public String landmarkLiked(@RequestBody Map<Object, Object> map) throws Exception {
+		String id = (String)map.get("id");
+		int lNo = Integer.parseInt((String)map.get("lNo"));
+		
+		landmarkService.landmarkLiked(id, lNo);
+		return null;
+	}
+	
+	
 	
 	
 	
--- src/travelMaker/model/dao/LandmarkBoardDAOImpl.java
+++ src/travelMaker/model/dao/LandmarkBoardDAOImpl.java
@@ -1,7 +1,8 @@
 package travelMaker.model.dao;
 
-import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,37 +16,46 @@ public class LandmarkBoardDAOImpl implements LandmarkBoardDAO{
 	private SqlSessionTemplate sqlSession = null;
 	
 	@Override
-	public void insertLand(LandmarkBoardDTO dto) throws SQLException {
+	public void insertLand(LandmarkBoardDTO dto) throws Exception {
 		// sqlSession 불러서 insert문 ("landmarkBoard는 뒤에 sql 매퍼에 이름 붙인놈이고 insertLand는 보드dao에 만들어준 이름?? or 뒤에 인서트 id ??")
 		sqlSession.insert("landmarkBoard.insertLand", dto); // 아 sql 매퍼에 landmarBoard라는 이름안에 insertLand id를 가진 놈을 부르는거구나
 	}
 	
 	@Override
-	public List getLands() throws SQLException {
+	public List getLands() throws Exception {
 		List land =  sqlSession.selectList("landmarkBoard.getLands");
 		return land;
 	}
 
 	@Override
-	public void getLandOpen(int lOpen) throws SQLException {
+	public void getLandOpen(int lOpen) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
 	
 	@Override
-	public List myLand(String id) throws SQLException{
+	public List myLand(String id) throws Exception{
 		List myLand =  sqlSession.selectList("landmarkBoard.myLand", id);
 		return myLand;
 	}
 
 	@Override
-	public LandmarkBoardDTO getLand(int lNo) throws SQLException {
+	public LandmarkBoardDTO getLand(int lNo) throws Exception {
 		LandmarkBoardDTO land =  sqlSession.selectOne("landmarkBoard.getLand", lNo);
 		return land;
 	}
 	
-	public void deletemyLand(String id) throws SQLException{
+	public void deletemyLand(String id) throws Exception{
 		sqlSession.delete("landmarkBoard.deletemyLand", id);
+	}
+
+	@Override
+	public void landmarkLikedCnt(int lNo, int ud) throws Exception {
+		Map map = new HashMap();
+		map.put("lNo", lNo);
+		map.put("ud", ud);
+		sqlSession.update("landmarkBoard.landmarkLikedCnt", map);
+		
 	}
 
 }
